a3c56af4f6228aeeae24afed9d0c858753537dfc
Jpdsf <jpdsafg@gmail.com>
2026-03-22 19:09:36 -0300

    feat(lab05): adiciona configurações para o tokenizer, incluindo modelo, comprimento máximo e ID de padding

--- lab_05/config.py
+++ lab_05/config.py
@@ -4,3 +4,8 @@ DATA_SRC_LANG = "en"
 DATA_TGT_LANG = "de"
 DATA_DATASET_NAME = "bentrevett/multi30k"
 DATA_SPLIT = "train"
+
+# Tokenizer
+TOKENIZER_MODEL = "bert-base-multilingual-cased"
+TOKENIZER_MAX_LENGTH = 64
+TOKENIZER_PAD_ID = 0
